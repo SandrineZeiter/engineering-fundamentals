d271d753ee990ffeadc7a19efe7eceddaa2b6c8f
SandrineZeiter <84182781+SandrineZeiter@users.noreply.github.com>
2026-04-14 09:50:22 +0000

    Correct app version

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,11 @@ import Counter from "./Counter";
 
 function App() {
 
-  const unusedVariable = 123; // ❌ Sonar: Unused variable
-
-  if (true) {
-    console.log("debug"); // ❌ Sonar: console statement / bad practice
-  }
 
   return (
     <>
       <Logo />
       <Counter />
-      <div onclick="alert('test')">Click me</div> 
-      {/* ❌ Sonar: invalid React event handler + accessibility issue */}
     </>
   );
 }
